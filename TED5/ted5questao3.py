27f509f519486e5f4ef5081906ffd6e8fa27c2c2
Jessica Neves <118688096+jessic4neves@users.noreply.github.com>
2023-04-09 15:05:03 -0300

    Delete TED5 directory

--- TED5/ted5questao3.py
+++
@@ -1,5 +0,0 @@
-amigos = ["Gabi", "Benny", "Thais", "Cynara"]
-print(amigos[0])
-print(amigos[1])
-print(amigos[2])
-print(amigos[3])
--- TED5/ted5questao5.py
+++
@@ -1,3 +0,0 @@
-amigos = ["Gabi", "Benny", "Thais", "Cynara"]
-for amigo in amigos:
-    print('Olá, como vai você',amigo,'?')
